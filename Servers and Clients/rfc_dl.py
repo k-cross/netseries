6bcaed6b6f9607276f9911399a1ea1b3042dfcff
ken <kenneth.cross@sjsu.edu>
2016-08-02 23:45:22 -0700

    added server and modified clients for functionality

--- Servers and Clients/rfc_dl.py
+++ Servers and Clients/rfc_dl.py
@@ -1,4 +1,4 @@
-import sys, urllib.request
+import sys
 import socket
 
 try:
@@ -7,9 +7,35 @@ except (IndexError, ValueError):
     print('Must supply an RFC number as first argument')
     sys.exit(2)
 
-template = 'http://www.ietf.org/rfc/rfc{}.txt'
-url = template.format(rfc_number)
-rfc_raw = urllib.request.urlopen(url).read()
-rfc = rfc_raw.decode()
+target_host = 'www.ietf.org'
+target_port = 80
+
+sock = socket.create_connection((target_host, target_port))
+
+req = (
+        'GET /rfc/rfc{rfcnum}.txt HTTP/1.1\r\n'
+        'Host: {target_host}:{target_port}\r\n'
+        'User-Agent: Python {version}\r\n'
+        'Connection: close\r\n'
+        '\r\n'
+)
+
+req = req.format(
+        rfcnum = rfc_number,
+        target_host = target_host,
+        target_port = target_port,
+        version = sys.version_info[0]
+)
+
+sock.sendall(req.encode('ascii'))
+
+rfc_raw = bytearray()
+while True:
+    buf = sock.recv(4096)
+    if not len(buf):
+        break
+    rfc_raw += buf
+
+rfc = rfc_raw.decode('utf-8')
 
 print(rfc)
--- Servers and Clients/tcp_client.py
+++ Servers and Clients/tcp_client.py
@@ -6,12 +6,20 @@ Assumptions:
 """
 import socket
 
-target_host = "www.google.com"
+target_host = 'www.google.com'
 target_port = 80
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((target_host, target_port))
-client.send("GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+client.send('GET / HTTP/1.1\r\nHost: google.com\r\n\r\n'.encode('ascii'))
+
+"""
+The create_connection is a convienece function for TCP connections
+The sendall below can be used for larger texts/requests and lists:
+
+client = socket.create_connection((target_host, target_port))
+client.sendall('GET / HTTP/1.1\r\nHost: google.com\r\n\r\n'.encode('ascii'))
+"""
 
 response = client.recv(4096)
 
