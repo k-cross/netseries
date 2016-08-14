f72d6e22719991b20bd82a6d5d847235a6b78652
ken <kenneth.cross@sjsu.edu>
2016-08-13 21:42:46 -0700

    sniffer functions

--- Passive/basic_sniffer.py
+++ Passive/basic_sniffer.py
@@ -15,8 +15,8 @@ class IP(Structure):
             ('ttl', c_ubyte),
             ('protocol_num', c_ubyte),
             ('sum', c_ushort),
-            ('src', c_ulong),
-            ('dst', c_ulong)
+            ('src', c_uint32), 
+            ('dst', c_uint32)
     ]
 
     def __new__(self, socket_buffer=None):
@@ -27,8 +27,14 @@ class IP(Structure):
         self.protocol_map = {1:'ICMP', 6:'TCP', 17:'UDP'}
 
         # Human readable IP addresses
-        self.src_address = socket.inet_ntoa(struct.pack('<L', self.src))
-        self.dst_address = socket.inet_ntoa(struct.pack('<L', self.dst))
+        self.src_address = socket.inet_ntoa(struct.pack('@I', self.src))
+        self.dst_address = socket.inet_ntoa(struct.pack('@I', self.dst))
+
+        # Human readable protocol
+        try:
+            self.protocol = self.protocol_map[self.protocol_num]
+        except:
+            self.protocol = str(self.protocol_num)
 
 
 # Host to listen on
@@ -50,19 +56,27 @@ def main():
         sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
         sniffer.bind((host, 0))
         sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1) # Includes IP headers in capture
+
         # If using Windows, send IOCTL to setup promiscuous mode
         if os.name == 'nt':
             sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
     
-        # Read in a single packet
-        print(sniffer.recvfrom(65565))
-    
+        while True:
+            # Read in a single packet
+            raw_buffer = sniffer.recvfrom(65565)[0]
+
+            # Create IP header from the buffer's first 20 bytes, passing in 32 bytes because of from_buffer_copy error
+            ip_header = IP(raw_buffer)
+
+            # Print detected host and protocol
+            print('Protocol: %s %s -> %s' % (ip_header.protocol, ip_header.src_address, ip_header.dst_address))
+
+    except KeyboardInterrupt:
         # Turn off promiscuous mode for Windows
         if os.name == 'nt':
             sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
-
-    except Exception:
-        print('[!] Error occurred: exiting!')
+ 
+        print('[!] Exiting!')
         sys.exit(1)
 
 if __name__ == '__main__':
